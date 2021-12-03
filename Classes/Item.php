a7f4912fde0a639779f4d96066b2ee0b49c2d041
Gurjinder <70706370+GakhalSingh@users.noreply.github.com>
2021-12-03 14:58:59 +0100

    getItems

--- Classes/Item.php
+++ Classes/Item.php
@@ -1,6 +1,4 @@
 <?php
-
-
 class Item
 {
     public $id;
--- index.php
+++ index.php
@@ -1,22 +1,45 @@
+<?php
+    require 'Modules/Database.php';
+    require 'Modules/Items.php';
+?>
 <!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <title>Zuzu Sushi</title>
+    </head>
+    <body >
+    <div class="container" style="background:pink">
+        <?php
+            include_once('defaults/header.php');
+            include_once('defaults/carousel.php');
+        ?>
+        <div class="row gy-3 ">
+            <?php $items = getItems(); ?>
+            <?php foreach ($items as $item) : ?>
+                <div class="col-sm-6 col-md-6">
+                    <div class="card" style="width: 35rem; height: 35rem">
+                        <div class="card-body text-center">
+                            <div class="card-title mb-3">
+                                <?= $item->name; ?>
+                            </div>
+                            <div>
+                                <a href="/machine/<?= $item->id ?>">
+                                    <img class="product-img img-responsive center-block" src='<?= $item->image ?>' />
+                                </a>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body >
-  <div class="container" style="background:pink">
-    <?php
-        include_once('defaults/header.php');
-        include_once('defaults/carousel.php');
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
     </body>
+    <?php
+    include_once('defaults/footer.php');
+    ?>
 </html>
